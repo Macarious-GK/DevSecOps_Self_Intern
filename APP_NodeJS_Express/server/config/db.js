ce0690661c11de408b975148511d671dfe6cb30b
MacariousGK <m.labibebidallah@nu.edu.eg>
2025-04-10 10:23:41 +0200

    update db.js

--- APP_NodeJS_Express/server/config/db.js
+++ APP_NodeJS_Express/server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test' || 'mongodb://mongodb:27017/test' );
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
